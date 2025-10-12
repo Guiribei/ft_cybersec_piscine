a2a9f139d7af3081880e4f84d14abb56e3ac72bf
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2025-10-12 18:12:36 -0300

    implement unit tests for -r
    Co-authored-by: Rods <rodrigogregoldo@gmail.com>

---
+++ tests.py
@@ -0,0 +1,24 @@
+from spider import main
+
+test_cases = [
+	{
+		"args": [],
+		"expected": (False, 1, None, "./data/")
+	},
+	{
+		"args": ["-r"],
+		"expected": (True, 1, None, "./data/")
+	},
+	{
+		"args": ["r"],
+		"expected": (False, 1, None, "./data/")
+	},
+]
+
+for i, case in enumerate(test_cases):
+	actual = main(case["args"])
+	try:
+		assert actual == case["expected"], f"Expected {case['expected']}, got {actual}"
+		print(f"Test case {i}: OK!")
+	except AssertionError as e:
+		print(f"Test case {i}: NOK! {e}")
